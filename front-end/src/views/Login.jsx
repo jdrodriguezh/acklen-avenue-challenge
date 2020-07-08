df07877738bf101ab803975992c9fbd37330909c
Josue <josue.rodriguez1551@gmail.com>
2020-07-08 14:05:08 -0600

    Finishing the login view

--- front-end/src/views/Login.jsx
+++ front-end/src/views/Login.jsx
@@ -1,19 +1,64 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import "../assets/Login.css";
+import React, { useState } from "react";
+import { Container, Card, Form, Button, Row, Col } from "react-bootstrap";
+import "../assets/Auth.css";
 
 const Login = () => {
-  return(
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  return (
     <Container className="main-container" fluid>
-      <Row className="justify-content-md-center">
-        <Col>
-          <div>
-            <p>Hols</p>
-          </div>
-        </Col>
-      </Row>
+      <Card className="card-container">
+        <Card.Body>
+          <Card.Title>Login</Card.Title>
+          <Form>
+            <Form.Group>
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                onChange={(evt) => {
+                  setUsername(evt.target.value);
+                }}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(evt) => {
+                  setPassword(evt.target.value);
+                }}
+              />
+            </Form.Group>
+          </Form>
+        </Card.Body>
+        <Card.Footer className="card-footer">
+          <Container>
+            <Row>
+              <Col className="center-content">
+                <Button
+                  className="submit-button"
+                  onClick={() => {
+                    console.log(username);
+                    console.log(password);
+                  }}>
+                  Submit
+                </Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Text>
+                  Don't have an account? <a href="/home">Create one!</a>
+                </Form.Text>
+              </Col>
+            </Row>
+          </Container>
+        </Card.Footer>
+      </Card>
     </Container>
   );
-}
+};
 
 export default Login;
