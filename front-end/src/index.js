f4ec10b639c06905bea03cb29e68fb8d28bbec51
Josue <josue.rodriguez1551@gmail.com>
2020-07-08 13:18:46 -0600

    Started the login view

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/index.css';
-import App from './views/App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/index.css";
+import App from "./views/App";
+import Home from "./views/Home";
+import Login from "./views/Login"
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/home" component={Home} />
+      <Route path="/login" component={Login}/>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ front-end/src/views/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "../assets/Login.css";
+
+const Login = () => {
+  return(
+    <Container className="main-container" fluid>
+      <Row className="justify-content-md-center">
+        <Col>
+          <div>
+            <p>Hols</p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Login;
