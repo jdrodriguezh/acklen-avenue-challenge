3325bb6207de1e7008a35960eb49213d64778fa2
Josue <josue.rodriguez1551@gmail.com>
2020-07-11 16:00:58 -0600

    Fixing bugs

--- client/src/views/Variables.js
+++ client/src/views/Variables.js
@@ -1,7 +1,7 @@
 //Development
-const BASE_URL = "http://localhost:8080/";
+//const BASE_URL = "http://localhost:8080/";
 
 //Deployed
-//const BASE_URL = "http://159.89.156.23:8080/";
+const BASE_URL = "/";
 
 export default BASE_URL;
